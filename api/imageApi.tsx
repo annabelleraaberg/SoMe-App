f5d8485a64aad73d1e0b8cb522f0178737ea3794
annabelleraaberg <129503939+annabelleraaberg@users.noreply.github.com>
2025-06-15 00:06:32 +0200

    added more logging

--- api/imageApi.tsx
+++ api/imageApi.tsx
@@ -1,12 +1,13 @@
 // Lecture code: https://github.com/studBrage/Kryssplattform-HK-H24/blob/main/api/imageApi.ts
 
 import { getStorageRef } from "@/firebaseConfig";
-import { uploadBytesResumable } from "firebase/storage";
+import { uploadBytes, uploadBytesResumable } from "firebase/storage";
 
 export const uploadImageToFirebase = async (uri: string) => {
   const fetchResponse = await fetch(uri);
   const blob = await fetchResponse.blob();
 
+  console.log("Uri: ", uri);
   const imagePath = uri.split("/").pop()?.split(".")[0] ?? "anonymtBilde";
   console.log("imagepath", imagePath);
 
@@ -16,7 +17,9 @@ export const uploadImageToFirebase = async (uri: string) => {
 
   try {
     console.log("pls");
-    await uploadBytesResumable(imageRef, blob);
+    console.log("Blob size:", blob.size); 
+    await uploadBytes(imageRef, blob);
+    //await uploadBytesResumable(imageRef, blob);
     console.log("Uploading image to", uploadPath);
     return uploadPath;
   } catch (e) {
